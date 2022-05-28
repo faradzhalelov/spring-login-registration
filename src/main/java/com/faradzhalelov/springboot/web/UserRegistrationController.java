1e836a516d3170a9af62327313e34bd76b24c759
faradzhalelov <faradzhalelov@gmail.com>
2022-05-28 16:46:47 +0400

    - Front-end implementation for Registration Feature(Thymeleaf template)

--- src/main/java/com/faradzhalelov/springboot/web/UserRegistrationController.java
+++ src/main/java/com/faradzhalelov/springboot/web/UserRegistrationController.java
@@ -3,22 +3,37 @@ package com.faradzhalelov.springboot.web;
 import com.faradzhalelov.springboot.service.UserService;
 import com.faradzhalelov.springboot.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/registration")
 public class UserRegistrationController {
 
-    private UserService userService;
+    private final UserService userService;
 
     public UserRegistrationController(UserService userService) {
         super();
         this.userService = userService;
     }
 
+    @ModelAttribute("user")
+    public UserRegistrationDto userRegistrationDto() {
+        return new UserRegistrationDto();
+    }
+
+    @GetMapping
+    public String showRegistrationForm() {
+        return "registration";
+    }
+
+    @PostMapping
     public String registerUserAccount(@ModelAttribute("user") UserRegistrationDto registrationDto) {
         userService.save(registrationDto);
         return "redirect:/registration?success";
     }
+
 }
