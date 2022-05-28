1baedc333900229c82d9f1dc137545108d14f1c9
faradzhalelov <faradzhalelov@gmail.com>
2022-05-28 13:56:04 +0400

    first commit

---
+++ src/main/java/com/faradzhalelov/springboot/RegistrationLoginSpringbootSecurityThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.faradzhalelov.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrationLoginSpringbootSecurityThymeleafApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegistrationLoginSpringbootSecurityThymeleafApplication.class, args);
+    }
+
+}
