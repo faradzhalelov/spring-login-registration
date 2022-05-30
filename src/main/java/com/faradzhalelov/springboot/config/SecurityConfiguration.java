ec5346f928574e349a0606c1301906316072bd3f
faradzhalelov <faradzhalelov@gmail.com>
2022-05-30 18:35:19 +0400

    - Logout and Home Page Implementation

--- src/main/java/com/faradzhalelov/springboot/config/SecurityConfiguration.java
+++ src/main/java/com/faradzhalelov/springboot/config/SecurityConfiguration.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -41,7 +40,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers(
-                        "/registration**",
+                "/registration**",
                         "/js/**",
                         "/css/**",
                         "/img/**").permitAll()
--- src/main/java/com/faradzhalelov/springboot/web/UserRegistrationController.java
+++ src/main/java/com/faradzhalelov/springboot/web/UserRegistrationController.java
@@ -3,7 +3,6 @@ package com.faradzhalelov.springboot.web;
 import com.faradzhalelov.springboot.service.UserService;
 import com.faradzhalelov.springboot.web.dto.UserRegistrationDto;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
